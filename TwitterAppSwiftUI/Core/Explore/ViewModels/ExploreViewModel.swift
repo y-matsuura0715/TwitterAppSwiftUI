ea49da1dfff0cca9a6d1b50d7382e76ed3019ddd
y-matsuura0715 <y-matsuura0715@outlook.jp>
2022-09-25 17:44:29 +0900

    Search Bar & FIlter Users

--- TwitterAppSwiftUI/Core/Explore/ViewModels/ExploreViewModel.swift
+++ TwitterAppSwiftUI/Core/Explore/ViewModels/ExploreViewModel.swift
@@ -9,7 +9,22 @@ import Foundation
 
 class ExploreViewModel: ObservableObject {
     @Published var users = [User]()
+    @Published var searchText = ""
+    
+    var searchableUsers: [User] {
+        if searchText.isEmpty {
+            return users
+        } else {
+            let lowercasedQuery = searchText.lowercased()
+            
+            return users.filter({
+                $0.username.contains(lowercasedQuery) || $0.fullname.contains(lowercasedQuery)
+            })
+        }
+    }
+    
     let service = UserService()
+    
     
     init() {
         fetchUsers()
